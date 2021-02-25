7eefab412a966040f65ee72b3f9f24a86e72bcc5
Grace Kabeya <antoinefaith1@gmail.com>
2021-02-25 11:50:13 +0100

    fortune teller

--- ex-1/exercise.js
+++ ex-1/exercise.js
@@ -1,2 +1,8 @@
 // Example
-window.alert("Hello World!");
+var numberOfChild = 3;
+  var partner = 'Giorgia';
+  var location = 'milano';
+  var jobTitle = 'developer';
+
+  console.log('you will be a:', jobTitle, 'in :' , location, 'married to :', partner, 'number of child :', numberOfChild);
+ window.alert('you will be a:', jobTitle, 'in :' , location, 'married to :', partner, 'number of child :', numberOfChild);
