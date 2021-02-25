ecec7e191de28e6ebe8b9563492a2c419aca584f
Grace Kabeya <antoinefaith1@gmail.com>
2021-02-25 16:20:36 +0100

    finished

--- ex-6/exercise.js
+++ ex-6/exercise.js
@@ -1,2 +1,6 @@
 // Example
-window.alert("Hello World!");
+var authorName;
+var authorSays;
+authorName = window.prompt('enter an author name generate a quote')
+authorSays = window.prompt('enter an author name generate a quote')
+window.alert(authorName +" "+ 'says' +" "+ authorSays)
