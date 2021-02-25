1fb65b54901cf55e33d7f72fca4203999bc67fe4
Grace Kabeya <antoinefaith1@gmail.com>
2021-02-25 12:19:51 +0100

    the greater number

--- ex-3/exercise.js
+++ ex-3/exercise.js
@@ -1,2 +1,10 @@
 // Example
-window.alert("Hello World!");
+var x = 5 
+var y = 10
+
+if (x < y){
+    console.log('The greater number of 5 and 10 is 10.')
+} else {
+    console.log('The numbers are the same.')
+}
+
